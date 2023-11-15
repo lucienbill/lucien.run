e1d114b165c19785aef322de4d7d0f98151eddf1
Lucien Bill <lucien.bill@gmail.com>
2023-11-15 23:29:14 +0100

    V1.0.0 ready

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # lucien.run
 personnal redirect thingy (URL minifier)
 
-v0.0.0 : just pure HTML
-future versions : a HTML files builder, and a yaml or json database of redirects
+The redirects work with pure HTML.
+
+The redirects are configured in `redirects.yml`, and the script to build them is `node builder/build.js`.
+The script also copies `index.html` to the root of the website
---
+++ builder/build.js
@@ -0,0 +1,40 @@
+const YAML = require('yaml')
+const fs = require('fs')
+
+const file = fs.readFileSync('redirects.yml', 'utf8')
+const yamlData = YAML.parse(file)
+
+const defaultLang = yamlData['default-lang']
+const redirects = yamlData.redirects
+const shortPaths = Object.keys(redirects)
+
+// write all redirections (for is faster than foreach)
+for (let index = 0; index < shortPaths.length; index++) {
+    const shortPath = shortPaths[index]
+    const redirect = redirects[shortPath];
+    const lang = (!!redirect.lang ? redirect.lang : defaultLang)
+    const filePath = `redirects/${shortPath}/index.html`
+    
+    const fileContent = `<!DOCTYPE html>
+<html lang="${lang}">
+    <head>
+    <meta charset="utf-8">
+    <title>${redirect.title}</title>
+    <meta property="title" content="${redirect.title}">
+    <meta name="description" content="${redirect.description}">
+    
+    <meta property="og:title" content="${redirect.title}">
+    <meta property="og:description" content="${redirect.description}">
+    <meta http-equiv="Refresh" content="0; url='${redirect.url}" />
+    <meta name="robots" content="noindex">
+    </head>
+</html>
+`
+    fs.writeFileSync(filePath, fileContent);
+}
+
+// write the index to the lucien.run/index.html
+fs.copyFile('index.html', 'redirects/index.html', (err) => {
+    if (err) throw err;
+    console.log('index.html was copied to redirects/index.html');
+});
